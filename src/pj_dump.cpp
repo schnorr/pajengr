20c8c1f280a90bbce06b41cb29b6820afff90512
Lucas M. Schnorr <schnorr@inf.ufrgs.br>
2018-11-07 07:37:02 -0200

    local variables to avoid retaining state between calls

--- src/pj_dump.cpp
+++ src/pj_dump.cpp
@@ -32,6 +32,47 @@ bool quiet = false;
 
 List _dump (PajeComponent *simulator)
 {
+  struct {
+    CharacterVector nature;
+    CharacterVector container;
+    CharacterVector type;
+    DoubleVector start;
+    DoubleVector end;
+    DoubleVector duration;
+    IntegerVector imbrication;
+    CharacterVector value;
+  } st;
+
+  struct {
+    CharacterVector nature;
+    CharacterVector container;
+    CharacterVector type;
+    DoubleVector start;
+    DoubleVector end;
+    DoubleVector duration;
+    DoubleVector value;
+  } va;
+
+  struct {
+    CharacterVector nature;
+    CharacterVector container;
+    CharacterVector type;
+    DoubleVector start;
+    CharacterVector value;
+  } ev;
+
+  struct {
+    CharacterVector nature;
+    CharacterVector container;
+    CharacterVector type;
+    DoubleVector start;
+    DoubleVector end;
+    DoubleVector duration;
+    CharacterVector value;
+    CharacterVector startContainer;
+    CharacterVector endContainer;
+    CharacterVector key;
+  } li;
   
   double si = simulator->startTime();
   double ei = simulator->endTime();
@@ -59,23 +100,78 @@ List _dump (PajeComponent *simulator)
 	entities = simulator->enumeratorOfEntitiesTypedInContainer (t, c, si, ei);
 	for(PajeEntity *e : entities){
 	  if (t->nature() == PAJE_StateType){
-	    _add_state_entity (e);
+	    st.nature.push_back("State");
+	    st.container.push_back(e->container()? e->container()->name() : "NULL");
+	    st.type.push_back(e->type()? e->type()->name() : "NULL");
+	    st.start.push_back(e->startTime());
+	    st.end.push_back(e->endTime());
+	    st.duration.push_back(e->duration());
+	    st.imbrication.push_back(e->imbricationLevel());
+	    st.value.push_back(e->value()? e->value()->name() : "NULL");
 	  }else if(t->nature() == PAJE_VariableType){
-	    _add_variable_entity (e);
+	    va.nature.push_back("Variable");
+	    va.container.push_back(e->container()? e->container()->name() : "NULL");
+	    va.type.push_back(e->type()? e->type()->name() : "NULL");
+	    va.start.push_back(e->startTime());
+	    va.end.push_back(e->endTime());
+	    va.duration.push_back(e->duration());
+	    va.value.push_back(e->doubleValue());
 	  }else if(t->nature() == PAJE_EventType){
-	    _add_event_entity (e);
+	    ev.nature.push_back("Variable");
+	    ev.container.push_back(e->container()? e->container()->name() : "NULL");
+	    ev.type.push_back(e->type()? e->type()->name() : "NULL");
+	    ev.start.push_back(e->startTime());
+	    ev.value.push_back(e->value()? e->value()->name() : "NULL");
 	  }else if(t->nature() == PAJE_LinkType){
-	    _add_link_entity (e);
+	    li.nature.push_back("State");
+	    li.container.push_back(e->container()? e->container()->name() : "NULL");
+	    li.type.push_back(e->type()? e->type()->name() : "NULL");
+	    li.start.push_back(e->startTime());
+	    li.end.push_back(e->endTime());
+	    li.duration.push_back(e->duration());
+	    li.value.push_back(e->value()? e->value()->name() : "NULL");
+	    li.startContainer.push_back(e->startContainer()? e->startContainer()->name() : "NULL");
+	    li.endContainer.push_back(e->endContainer()? e->endContainer()->name() : "NULL");
+	    li.key.push_back(e->key());
 	  }
 	}
       }
     }
   }
-
-  return List::create(_collect_state_dataframe(),
-		      _collect_variable_dataframe(),
-		      _collect_event_dataframe(),
-		      _collect_link_dataframe());
+  DataFrame stDF = DataFrame::create(Named("Nature") = st.nature,
+				     Named("Container") = st.container,
+				     Named("Type") = st.type,
+				     Named("Start") = st.start,
+				     Named("End") = st.end,
+				     Named("Duration") = st.duration,
+				     Named("Imbrication") = st.imbrication,
+				     Named("Value") = st.value);
+  DataFrame vaDF = DataFrame::create(Named("Nature") = va.nature,
+				     Named("Container") = va.container,
+				     Named("Type") = va.type,
+				     Named("Start") = va.start,
+				     Named("End") = va.end,
+				     Named("Duration") = va.duration,
+				     Named("Value") = va.value);
+  DataFrame evDF = DataFrame::create(Named("Nature") = ev.nature,
+				     Named("Container") = ev.container,
+				     Named("Type") = ev.type,
+				     Named("Start") = ev.start,
+				     Named("Value") = ev.value);
+  DataFrame liDF = DataFrame::create(Named("Nature") = li.nature,
+				     Named("Container") = li.container,
+				     Named("Type") = li.type,
+				     Named("Start") = li.start,
+				     Named("End") = li.end,
+				     Named("Duration") = li.duration,
+				     Named("Value") = li.value,
+				     Named("StartContainer") = li.startContainer,
+				     Named("EndContainer") = li.endContainer,
+				     Named("Key") = li.key);
+  return List::create(Named("state") = stDF,
+		      Named("variable") = vaDF,
+		      Named("event") = evDF,
+		      Named("link") = liDF);
 }
 
 // [[Rcpp::export]]
